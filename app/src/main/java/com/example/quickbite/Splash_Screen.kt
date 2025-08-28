2b30eec5e1c7bb44a9055d357d4026b15f0d859a
ByteSlayyer <deathblade@192.168.1.3>
2025-08-29 00:22:53 +0530

    Implemented Splash Screen!

--- app/src/main/java/com/example/quickbite/Splash_Screen.kt
+++ app/src/main/java/com/example/quickbite/Splash_Screen.kt
@@ -1,11 +1,14 @@
 package com.example.quickbite
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+@Suppress("DEPRECATION")
 class Splash_Screen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,5 +19,10 @@ class Splash_Screen : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },3000)
     }
 }
